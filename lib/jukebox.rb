b51c9ef1b23224b008ab406d7b06e80e12b43a09
Renée Cruz <reneebasscruz@gmail.com>
2019-08-04 22:08:31 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -23,6 +23,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_response_input = gets.strip
+  binding.pry
   
   songs.each_with_index do |index,song_name|
     if user_response_input == song_name 
