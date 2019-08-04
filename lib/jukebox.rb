9cefd35ce9d2612cceef9887dc7e6f6874f9412b
Renée Cruz <reneebasscruz@gmail.com>
2019-08-04 21:54:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -23,7 +23,7 @@ def play(songs)
   user_response_input = gets.strip
   
   songs.each_with_index do |index,song_name|
-    if user_response_input == song_name || songs[user_response_input.to_i]
+    if user_response_input == song_name || songs[user_response_input.to_i+1]
       puts "Playing #{song_name}"
     else 
       puts "Invalid input, please try again."
